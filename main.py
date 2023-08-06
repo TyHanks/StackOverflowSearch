377220af371318aebabfb76b6f601af1cbc69ea2
TyHanks <mudlight48@icloud.com>
2023-08-05 23:08:19 -0400

    added header and moved search input

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from scripts.search import search
 from scripts.data import *
+from pyfiglet import Figlet
+
+#* Application Header
+f = Figlet(font='slant')
+print(f.renderText('Stack-Search'))
 
 passSearch = search()
 passData = getSearch(passSearch)
@@ -8,5 +13,3 @@ summaries = getSummaries(passData) #* summaries[0].string 0 thru 9
 urls = getUrls(passData)
 useableUrls = buildUrls(urls) #* useableUrls[0] 0 thru 9
 stats = getStats(passData) #* stats[0].string // 0,1,2 = votes,answers,views 3,4,5 = votes,answers,views etc...
-
-
