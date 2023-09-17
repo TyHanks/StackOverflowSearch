a7b03d199caafacc2ab276c8ac38fc4bd64767eb
TyHanks <mudlight48@protonmail.com>
2023-09-17 13:40:06 -0400

    commit branch update

--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,75 @@
-# StackOverflowSearch
+# StackOverFlowSearch - THanksDev
+
+![License](https://img.shields.io/pypi/l/your-package-name)
+
+An API for managing a online forum using disk-persistent storage and http requests.
+
+## Table of Contents
+
+* [About](#about)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Documentation](#documentation)
+* [License](#license)
+
+## About
+
+![image](img/screenshot.png)
+
+## Installation
+
+Install alpha version by using `git clone` ssh or html for testing purposes
+
+``` bash
+git clone git@github.com:TyHanks/StackOverflowSearch.git
+# or
+git clone https://github.com/TyHanks/StackOverflowSearch.git
+```
+
+Create a python enviorment `virtualenv env` is what I will use for example then switch to env
+
+``` bash
+# create env
+virtualenv env
+# activate env
+source env/bin/activate
+```
+
+You can install `StackOverFlowSearch` dependencies using `pip`. Make sure you have Python 3.7+ installed.
+
+``` bash
+pip install beautifulsoup4
+pip install clint
+pip install pyfiglet
+pip install requests
+```
+
+## Usage
+
+Will be added in the Beta Version
+
+## License
+
+``` bash
+MIT License
+
+Copyright (c) 2023 Ty Hanks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
--- main.py
+++ main.py
@@ -4,63 +4,168 @@ from scripts.data import *
 from pyfiglet import Figlet
 from clint.textui import prompt, puts, colored
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # *************************************************************************************************************#
+    # * Application Header
+    # *************************************************************************************************************#
+    f = Figlet(font="slant")
+    puts(colored.yellow(f.renderText("STACK-SEARCH")))
 
-    #*************************************************************************************************************#
-    #* Application Header
-    #*************************************************************************************************************#
-    f = Figlet(font='slant')
-    puts(colored.yellow(f.renderText('STACK-SEARCH')))
-
-    #*************************************************************************************************************#
-    #* Search Prompt / User input / data functions
-    #*************************************************************************************************************#
+    # *************************************************************************************************************#
+    # * Search Prompt / User input / data functions
+    # *************************************************************************************************************#
     search = prompt.query("What would you like to search for?: ")
     passData = getSearch(search)
     headings = getHeadings(passData)
     stats = getStats(passData)
     summaries = getSummaries(passData)
-    #?: Possible future feature - 1: urls = getUrls(passData)
-    #?: Possible future feature - 1: useableUrls = buildUrls(urls)
+    # ?: Possible future feature - 1: urls = getUrls(passData)
+    # ?: Possible future feature - 1: useableUrls = buildUrls(urls)
 
-    #*************************************************************************************************************#
-    #* Question / Header selection
-    #*************************************************************************************************************#
+    # *************************************************************************************************************#
+    # * Question / Header selection
+    # *************************************************************************************************************#
     question_options = [
-                    {'selector':'1','prompt': colored.cyan(headings[0].text) + 'Votes: ' + colored.yellow(stats[0].string) + ' Answers: ' + colored.yellow(stats[1].string) + ' Views: ' + colored.yellow(stats[2].string),'return':'0'},
-                    {'selector':'2','prompt': colored.cyan(headings[1].text) + 'Votes: ' + colored.yellow(stats[3].string) + ' Answers: ' + colored.yellow(stats[4].string) + ' Views: ' + colored.yellow(stats[5].string),'return':'1'},
-                    {'selector':'3','prompt': colored.cyan(headings[2].text) + 'Votes: ' + colored.yellow(stats[6].string) + ' Answers: ' + colored.yellow(stats[7].string) + ' Views: ' + colored.yellow(stats[8].string),'return':'2'},
-                    {'selector':'4','prompt': colored.cyan(headings[3].text) + 'Votes: ' + colored.yellow(stats[9].string) + ' Answers: ' + colored.yellow(stats[10].string) + ' Views: ' + colored.yellow(stats[11].string),'return':'3'},
-                    {'selector':'5','prompt': colored.cyan(headings[4].text) + 'Votes: ' + colored.yellow(stats[12].string) + ' Answers: ' + colored.yellow(stats[13].string) + ' Views: ' + colored.yellow(stats[14].string),'return':'4'},
-                    {'selector':'6','prompt': colored.cyan(headings[5].text) + 'Votes: ' + colored.yellow(stats[15].string) + ' Answers: ' + colored.yellow(stats[16].string) + ' Views: ' + colored.yellow(stats[17].string),'return':'5'},
-                    {'selector':'7','prompt': colored.cyan(headings[6].text) + 'Votes: ' + colored.yellow(stats[18].string) + ' Answers: ' + colored.yellow(stats[19].string) + ' Views: ' + colored.yellow(stats[20].string),'return':'6'},
-                    {'selector':'8','prompt': colored.cyan(headings[7].text) + 'Votes: ' + colored.yellow(stats[21].string) + ' Answers: ' + colored.yellow(stats[22].string) + ' Views: ' + colored.yellow(stats[23].string),'return':'7'},
-                    {'selector':'9','prompt': colored.cyan(headings[8].text) + 'Votes: ' + colored.yellow(stats[24].string) + ' Answers: ' + colored.yellow(stats[25].string) + ' Views: ' + colored.yellow(stats[26].string),'return':'8'},
-                    {'selector':'10','prompt': colored.cyan(headings[9].text) + 'Votes: ' + colored.yellow(stats[27].string) + ' Answers: ' + colored.yellow(stats[28].string) + ' Views: ' + colored.yellow(stats[29].string),'return':'9'},]
-    
-    question_selection = prompt.options("Select one of the questions!", question_options)
+        {
+            "selector": "1",
+            "prompt": colored.cyan(headings[0].text)
+            + "Votes: "
+            + colored.yellow(stats[0].string)
+            + " Answers: "
+            + colored.yellow(stats[1].string)
+            + " Views: "
+            + colored.yellow(stats[2].string),
+            "return": "0",
+        },
+        {
+            "selector": "2",
+            "prompt": colored.cyan(headings[1].text)
+            + "Votes: "
+            + colored.yellow(stats[3].string)
+            + " Answers: "
+            + colored.yellow(stats[4].string)
+            + " Views: "
+            + colored.yellow(stats[5].string),
+            "return": "1",
+        },
+        {
+            "selector": "3",
+            "prompt": colored.cyan(headings[2].text)
+            + "Votes: "
+            + colored.yellow(stats[6].string)
+            + " Answers: "
+            + colored.yellow(stats[7].string)
+            + " Views: "
+            + colored.yellow(stats[8].string),
+            "return": "2",
+        },
+        {
+            "selector": "4",
+            "prompt": colored.cyan(headings[3].text)
+            + "Votes: "
+            + colored.yellow(stats[9].string)
+            + " Answers: "
+            + colored.yellow(stats[10].string)
+            + " Views: "
+            + colored.yellow(stats[11].string),
+            "return": "3",
+        },
+        {
+            "selector": "5",
+            "prompt": colored.cyan(headings[4].text)
+            + "Votes: "
+            + colored.yellow(stats[12].string)
+            + " Answers: "
+            + colored.yellow(stats[13].string)
+            + " Views: "
+            + colored.yellow(stats[14].string),
+            "return": "4",
+        },
+        {
+            "selector": "6",
+            "prompt": colored.cyan(headings[5].text)
+            + "Votes: "
+            + colored.yellow(stats[15].string)
+            + " Answers: "
+            + colored.yellow(stats[16].string)
+            + " Views: "
+            + colored.yellow(stats[17].string),
+            "return": "5",
+        },
+        {
+            "selector": "7",
+            "prompt": colored.cyan(headings[6].text)
+            + "Votes: "
+            + colored.yellow(stats[18].string)
+            + " Answers: "
+            + colored.yellow(stats[19].string)
+            + " Views: "
+            + colored.yellow(stats[20].string),
+            "return": "6",
+        },
+        {
+            "selector": "8",
+            "prompt": colored.cyan(headings[7].text)
+            + "Votes: "
+            + colored.yellow(stats[21].string)
+            + " Answers: "
+            + colored.yellow(stats[22].string)
+            + " Views: "
+            + colored.yellow(stats[23].string),
+            "return": "7",
+        },
+        {
+            "selector": "9",
+            "prompt": colored.cyan(headings[8].text)
+            + "Votes: "
+            + colored.yellow(stats[24].string)
+            + " Answers: "
+            + colored.yellow(stats[25].string)
+            + " Views: "
+            + colored.yellow(stats[26].string),
+            "return": "8",
+        },
+        {
+            "selector": "10",
+            "prompt": colored.cyan(headings[9].text)
+            + "Votes: "
+            + colored.yellow(stats[27].string)
+            + " Answers: "
+            + colored.yellow(stats[28].string)
+            + " Views: "
+            + colored.yellow(stats[29].string),
+            "return": "9",
+        },
+    ]
 
-    #*************************************************************************************************************#
-    #* Question Selected Display
-    #*************************************************************************************************************#
-    puts(colored.yellow('-----------------------------------------------------------------'))
+    question_selection = prompt.options(
+        "Select one of the questions!", question_options
+    )
+
+    # *************************************************************************************************************#
+    # * Question Selected Display
+    # *************************************************************************************************************#
+    puts(
+        colored.yellow(
+            "-----------------------------------------------------------------"
+        )
+    )
 
     def choice_prompt():
         choice_options = [
-                    {'selector':'o','prompt': 'View Answers','return': 'o'},
-                    {'selector':'s','prompt': 'Search Again','return': 's'},
-                    {'selector':'q','prompt': 'Close / Quit','return':'quit'},
-                    ]
+            {"selector": "o", "prompt": "View Answers", "return": "o"},
+            {"selector": "s", "prompt": "Search Again", "return": "s"},
+            {"selector": "q", "prompt": "Close / Quit", "return": "quit"},
+        ]
         choice_selection = prompt.options("Select an option", choice_options)
         return choice_selection
-    
+
     def choice_prompt_actions(choice_selection, question_number):
-        if choice_selection == 'o':
-            ## View answers
-            #! Finish pulling data for answers before finishing this
-        elif choice_selection == 's':
+        if choice_selection == "o":
+            print("")
+        elif choice_selection == "s":
             ## Search again
             python = sys.executable
             os.execl(python, python, *sys.argv)
@@ -68,25 +173,36 @@ if __name__ == '__main__':
             ## Close application
             sys.exit(0)
 
-    if question_selection == '0':
+    if question_selection == "0":
         puts(colored.cyan(headings[0].text))
-        puts('Votes: ' + colored.yellow(stats[0].string) + ' Answers: ' + colored.yellow(stats[1].string) + ' Views: ' + colored.yellow(stats[2].string))
-        #?: Possible future feature - 1: puts(colored.blue(useableUrls[0]))
-        puts('Summary: ' + colored.cyan(summaries[0].string.lstrip()))
+        puts(
+            "Votes: "
+            + colored.yellow(stats[0].string)
+            + " Answers: "
+            + colored.yellow(stats[1].string)
+            + " Views: "
+            + colored.yellow(stats[2].string)
+        )
+        # ?: Possible future feature - 1: puts(colored.blue(useableUrls[0]))
+        puts("Summary: " + colored.cyan(summaries[0].string.lstrip()))
         choice_selection = choice_prompt()
         question_number = 0
         choice_action = choice_prompt_actions(choice_selection, question_number)
-    elif question_selection == '1':
+    elif question_selection == "1":
         puts(colored.cyan(headings[1].text))
-        puts('Votes: ' + colored.yellow(stats[3].string) + ' Answers: ' + colored.yellow(stats[4].string) + ' Views: ' + colored.yellow(stats[5].string))
-        #?: Possible future feature - 1: puts(colored.blue(useableUrls[0]))
-        puts('Summary: ' + colored.cyan(summaries[1].string.lstrip()))
+        puts(
+            "Votes: "
+            + colored.yellow(stats[3].string)
+            + " Answers: "
+            + colored.yellow(stats[4].string)
+            + " Views: "
+            + colored.yellow(stats[5].string)
+        )
+        # ?: Possible future feature - 1: puts(colored.blue(useableUrls[0]))
+        puts("Summary: " + colored.cyan(summaries[1].string.lstrip()))
         choice_selection = choice_prompt()
         question_number = 1
         choice_action = choice_prompt_actions(choice_selection, question_number)
         #!: Finish rest of question_selections 2, 3, 4, 5, 6, 7, 8, 9
     else:
-        puts('Try selection again')
-
-
-    
+        puts("Try selection again")
